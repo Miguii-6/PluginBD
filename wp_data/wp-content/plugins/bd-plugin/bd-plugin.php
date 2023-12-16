7926c04dd27de3041a1b36285fd6091e094c1ea0
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-12-16 22:45:17 +0100

    commit con insertarfilas

--- wp_data/wp-content/plugins/bd-plugin/bd-plugin.php
+++ wp_data/wp-content/plugins/bd-plugin/bd-plugin.php
@@ -25,7 +25,7 @@ function creartabla()
 
     $charset_collate = $wpdb->get_charset_collate();
 
-    $table_name = $wpdb->prefix . 'dam';
+    $table_name = $wpdb->prefix . 'dam';// Nombre de la tabla con prefijo de WordPress
 
     $sql = "CREATE TABLE $table_name (
      id mediumint(9) NOT NULL AUTO_INCREMENT,
@@ -42,3 +42,31 @@ function creartabla()
 }
 // Hook para ejecutar la función creartabla al cargar el plugin
 add_action('plugin_loaded', 'creartabla');
+
+
+// Función para insertar filas en la tabla creada
+function insertarfilas()
+{
+    global $eufemismos, $soeces, $wpdb;
+
+    $table_name = $wpdb->prefix . 'dam';
+
+    // Consulta para seleccionar registros de la tabla
+    $select_consulta = $wpdb->get_results("SELECT * FROM $table_name");
+
+    // Si no hay registros en la tabla, se insertan los valores de las palabras definidas
+    if (count($select_consulta) == 0) {
+        for ($i = 0; $i < count($eufemismos); $i++) {
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'tonterias' => $soeces[$i],
+                    'eufenismos' => $eufemismos[$i]
+                )
+            );
+        }
+    }
+}
+// Hook para ejecutar la función insertarfilas al cargar el plugin
+add_action('plugin_loaded', 'insertarfilas');
+
